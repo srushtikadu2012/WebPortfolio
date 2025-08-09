9958b679cf2bfbdbc828b3d06431bd16087c0f03
Srushti Kadu <srushtikadu2012@gmail.com>
2025-08-09 13:10:26 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const typed = new Typed('#typed-text', {
+  strings: ['Srushti_Kadu', 'Software Developer'],
+  typeSpeed: 60,
+  backSpeed: 30,
+  loop: true
+});
+
+// AOS init
+AOS.init({
+  duration: 1000,
+  once: true
+});
